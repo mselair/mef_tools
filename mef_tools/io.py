Fix int64 -> int for precision

mef_tools/io.py
@@ -246,9 +246,6 @@ class MefWriter:
                 True on success
         """
 
-        print('WWWWWWWWWWWWWWWWWWWw')
-        print(start_uutc, end_uutc)
-
         # infer end_uutc from data
         if end_uutc is None:
             end_uutc = int64(start_uutc + (len(data_write)/sampling_freq * 1e6))
@@ -269,7 +266,7 @@ class MefWriter:
                 print(' Sampling frequency of provided data does not match fs of already written data')
                 return None
             # read precision from metadata - scale factor / can be different in new segment but not implemented
-            precision = int64(-1 * np.log10(self.channel_info[channel]['ufact'][0]))
+            precision = int(-1 * np.log10(self.channel_info[channel]['ufact'][0]))
 
             # convert data to int32
             data_converted = convert_data_to_int32(data_write, precision=precision)
@@ -398,8 +395,6 @@ class MefWriter:
         return annot_list
 
     def _create_segment(self, data=None, channel=None, start_uutc=None, end_uutc=None, sampling_frequency=None, segment=0,):
-        print('SSSSSSSSSSSSSSSS')
-        print(start_uutc, end_uutc)
         if data.dtype != np.int32:
             raise AssertionError('[TYPE ERROR] - MEF file writer accepts only int32 signal datatype!')
 
@@ -438,9 +433,6 @@ class MefWriter:
                                                data)
 
     def _append_block(self, data=None, channel=None, start_uutc=None, end_uutc=None, segment=0):
-        print('CCCCCCCCCCCCCCCCC')
-        print(start_uutc, end_uutc)
-
         if end_uutc < start_uutc:
             raise ValueError('End uutc timestamp lower than the start_uutc')
         if self.verbose:

mef_tools/test/test_io.py
@@ -32,8 +32,6 @@ class TestMefWriter(TestCase):
         start_time = np.int64(1578715810000000)
         # define end of data in uUTC time
         end_time = np.int64(start_time + 1e6 * secs_to_write)
-        print('AAAAAAAAAAAAAA')
-        print(start_time, end_time)
 
         writer = self.mef_writer
         writer.max_nans_written = 100
@@ -52,18 +50,7 @@ class TestMefWriter(TestCase):
         read_data = writer.session.read_ts_channels_uutc(channel, [start_time, end_time])
         read_data_nans = np.isnan(read_data)
         write_data_nans = np.isnan(test_data_1)
-        print('XXXXXXXXXXX')
-        print(start_time, end_time)
-        print(read_data_nans.dtype, write_data_nans.dtype)
-        print(read_data_nans.shape, write_data_nans.shape)
-        print(test_data_1.dtype, read_data.dtype)
-        print(test_data_1.shape, read_data.shape)
-        print(test_data_1.min(), read_data.min())
-        print(test_data_1.max(), read_data.max())
-        print(np.nanmin(test_data_1), np.nanmin(read_data))
-        print(np.nanmax(test_data_1), np.nanmax(read_data))
-        print(test_data_1[::100])
-        print(read_data[::100])
+
 
         self.assertTrue(np.array_equal(read_data_nans, write_data_nans))
         self.assertTrue(check_data_integrity(test_data_1, read_data, precision))